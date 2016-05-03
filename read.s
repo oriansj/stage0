8ebf08d91ab8390fe5530dc167a64176d1be4cf0
Jeremiah Orians <jeremiah@pdp10.guru>
2016-05-02 20:47:57 -0400

    Converted xeh to Hex

---
+++ read.s
@@ -0,0 +1,76 @@
+.data
+# we must export the entry point to the ELF linker or loader.
+# They convientionally recognize _start as their entry point.
+# Use ld -e main to override the default if you wish
+.global _start
+
+_start:
+
+	# first check that we got the correct number of inputs
+	pop	%rax		# Get the number of arguments
+	pop	%rdi		# Get the program name
+	pop	%rdi		# Get the actual argument
+
+	# Check if we have the correct number of inputs
+	cmp	$2, %rax
+
+	# Jump to Bail if the number is not correct
+	jne	Bail
+
+	# attempt to open the file for reading
+	mov	$0, %rsi	# prepare read_only
+				# we already have what we need in ebx
+	mov $2, %rax		# the syscall number for open()
+	syscall		# call the Kernel
+
+	# Check if we have a valid file
+	test	%rax, %rax
+
+	# Jump to Bail_file if not actual file
+	js	Bail
+
+	mov %rax, %rdi	# move the pointer to the right location
+
+Circle:	#print contents of file
+
+	mov $read_size, %rdx	# set the size of chars we want
+	mov $buffer, %rsi	# Where to put it
+				# We already have what we need in ebx
+	mov $0, %rax		# the syscall number for read
+	syscall		# call the Kernel
+
+	test %rax, %rax	# check what we got
+	jz Done		# Got EOF call it done
+
+	# Make sure we don't write a bunch of NULLs
+	mov %rax, %rdx
+
+	# get file pointer out of the way
+	movq %rdi, %rsp
+
+				# edx was already setup
+	mov $1, %rdi		# setup stdout write
+	mov $1, %rax		# setup the write
+	syscall			# call the Kernel
+
+	#now to prepare for next loop
+	movq %rsp, %rdi
+	jmp Circle
+
+Done:
+	# program completed Successfully
+	mov	$0, %rdi	# All is well
+	mov	$60, %rax	# put the exit syscall number in eax
+	syscall		# Call it a good day
+
+Bail:
+	# terminate with an error
+	mov	$1, %rdi	# there was an error
+	mov	$60, %rax	# put the exit syscall number in eax
+	syscall		# bail out
+
+# Our writable space
+# 2^ 30 Should be enough per read
+read_size = 1073741824
+buffer:
+	.space 1
--- xeh1.s
+++ xeh1.s
@@ -1,7 +1,4 @@
-.text # section declaration
-output:	.byte 0x30, 0x31, 0x32, 0x33, 0x34, 0x35, 0x36, 0x37, 0x38, 0x39, 0x41, 0x42, 0x43, 0x44, 0x45, 0x46, 0x0A
-
-	
+.data
 # we must export the entry point to the ELF linker or loader.
 # They convientionally recognize _start as their entry point.
 # Use ld -e main to override the default if you wish
@@ -14,7 +11,7 @@ loop:
 	mov $input, %rsi	# Where to put it
 	mov $0, %rdi		# Where are we reading from
 	mov $0, %rax		# the syscall number for read
-	syscall			# call the Kernel
+	syscall		# call the Kernel
 
 	# If we didn't read any bytes jump to Done
 	test %rax, %rax		# check what we got
@@ -32,32 +29,29 @@ loop:
 	# add our base pointer
 	add $output, %r12	# Use that as our index into our array
 	add $output, %r13	# Use that as our index into our array
-	
+
 	# Print our first Hex
 	mov $1, %rdx		# set the size of chars we want
 	mov %r12, %rsi		# What we are writing
 	mov $1, %rdi		# Stdout File Descriptor
 	mov $1, %rax		# the syscall number for write
-	syscall			# call the Kernel
+	syscall		# call the Kernel
 
 	# Print our second Hex
 	mov $1, %rdx		# set the size of chars we want
 	mov %r13, %rsi		# What we are writing
 	mov $1, %rdi		# Stdout File Descriptor
 	mov $1, %rax		# the syscall number for write
-	syscall			# call the Kernel	
+	syscall		# call the Kernel
 	jmp loop
 
-Done:	
+Done:
 	# program completed Successfully
 	mov	$0, %rdi	# All is well
 	mov	$60, %rax	# put the exit syscall number in eax
-	syscall			# Call it a good day
-	
-.data
-	
+	syscall		# Call it a good day
+
 write_size = 2
+output: .byte 0x30, 0x31, 0x32, 0x33, 0x34, 0x35, 0x36, 0x37, 0x38, 0x39, 0x41, 0x42, 0x43, 0x44, 0x45, 0x46, 0x0A
 input:
 	.byte write_size
-
-
