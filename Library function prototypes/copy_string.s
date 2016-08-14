4ed9c427f2ba2e4ec0e620f1416aea0e2906f19e
Jeremiah Orians <jeremiah@pdp10.guru>
2016-08-14 17:41:16 -0400

    Initial prototype for a copy_string function

--- Library function prototypes/copy_string.s
+++ Library function prototypes/copy_string.s
@@ -1,14 +1,13 @@
-	# We will be using R0 to pass pointer to string
-	# to cleared by the function
+	# We will be using R0 and R1 to pass values to the function
 	# R15 will be used as the stack pointer
 :start
 	LOADUI R0 @string
-	LOADUI R1 22
+	LOADUI R1 0x100
 	LOADUI R2 33
 	LOADUI R3 44
 	LOADUI R4 55
 	LOADUI R15 0x600
-	CALLI R15 @clear_string
+	CALLI R15 @copy_string
 	HALT
 :string
 	HALT
@@ -16,21 +15,21 @@
 	NOP
 
 ;;  Our simple string compare function
-:clear_string
+:copy_string
 	;; Preserve registers
 	PUSHR R0 R15
 	PUSHR R1 R15
 	PUSHR R2 R15
 	PUSHR R3 R15
 	;; Setup registers
+	MOVE R2 R1
 	MOVE R1 R0
-	LOADUI R2 0
 	LOADUI R3 0
-:clear_byte
-	LOADXU8 R0 R1 R2			; Get the byte
-	STOREX8 R3 R1 R2			; Overwrite with a Zero
-	ADDUI R2 R2 1				; Prep for next loop
-	JUMP.NZ R0 @clear_byte		; Stop if byte is NULL
+:copy_byte
+	LOADXU8 R0 R1 R3			; Get the byte
+	STOREX8 R0 R2 R3			; Store the byte
+	ADDUI R3 R3 1				; Prep for next loop
+	JUMP.NZ R0 @copy_byte	; Stop if byte is NULL
 ;; Done
 	;; Restore registers
 	POPR R3 R15
