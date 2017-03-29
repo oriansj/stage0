5f6c7f82bc818266b8ab9708002d67594af74d93
Jeremiah Orians <jeremiah@pdp10.guru>
2017-03-28 20:25:39 -0400

    Added license to all source files

--- stage1/dehex.s
+++ stage1/dehex.s
@@ -1,3 +1,18 @@
+; This file is part of stage0.
+;
+; stage0 is free software: you can redistribute it and/or modify
+; it under the terms of the GNU General Public License as published by
+; the Free Software Foundation, either version 3 of the License, or
+; (at your option) any later version.
+;
+; stage0 is distributed in the hope that it will be useful,
+; but WITHOUT ANY WARRANTY; without even the implied warranty of
+; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+; GNU General Public License for more details.
+;
+; You should have received a copy of the GNU General Public License
+; along with stage0.  If not, see <http://www.gnu.org/licenses/>.
+
 :start
 	FALSE R14                   ; R14 will be storing our current address
 	LOADUI R15 $end             ; We will be using R15 for our stack
--- x86/stage1/stage1_loader.s
+++ x86/stage1/stage1_loader.s
@@ -1,3 +1,18 @@
+; This file is part of stage0.
+;
+; stage0 is free software: you can redistribute it and/or modify
+; it under the terms of the GNU General Public License as published by
+; the Free Software Foundation, either version 3 of the License, or
+; (at your option) any later version.
+;
+; stage0 is distributed in the hope that it will be useful,
+; but WITHOUT ANY WARRANTY; without even the implied warranty of
+; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+; GNU General Public License for more details.
+;
+; You should have received a copy of the GNU General Public License
+; along with stage0.  If not, see <http://www.gnu.org/licenses/>.
+
 BITS 16
 	;; Steps for building
 	;; nasm -o stage1_loader stage1_loader.s
