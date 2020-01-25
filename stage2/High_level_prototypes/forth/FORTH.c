d7cc0907ee83cae3cf52eabb144c07eaf49bd960
Jeremiah Orians <jeremiah@pdp10.guru>
2020-01-25 15:34:27 -0500

    Added missing High level prototypes for stage2

---
+++ stage2/High_level_prototypes/forth/FORTH.c
@@ -0,0 +1,4 @@
+no, I really am not going to waste time writing this.
+FORTH is not a real bootstrapping language.
+It never was and never will be.
+Any FORTH programmer who wants it is free to write it.
